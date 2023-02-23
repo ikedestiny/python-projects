eed6e0b9709f28ee47d8f7f0bed9a19a67074244
Ike Holy Destiny <ikedestiny18@gmail.com>
2023-02-23 18:20:17 +0300

    spotted the print bug and changed it to return, added perm and comb

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-#calculator functions for addition subtractionmultiplication and division, factorial....
+#calculator functions for addition subtractionmultiplication and division, factorial, permulation, combination.....
 
 class calculator:
     def __init__(self, a, b):
@@ -6,21 +6,38 @@ class calculator:
         self.b = b
 
     def add(a,b):
-        print(a+b)
+        return(a+b)
 
     def sub(a,b):
-        print(a-b)
+        return(a-b)
 
     def mult(a, b):
-        print(a*b)
+        return(a*b)
 
     def div(a,b):
-        print(a/b)
+        return(a/b)
 
     def factorial(a):
         fac = 1
-        for i in range (1, a+1):
-            fac *= i
-        print(fac)
+        if a >= 0:
+          for i in range(1, a+1):
+             fac *= i
+          return fac
+        else:
+            return ("MATHS ERROR")
+           
+    def comb(a,b):
+        if a >= b:
+            c = calculator.factorial(a-b)
+
+            return (calculator.factorial(a)/c/calculator.factorial(b))
+        else:
+            return("MATHS ERROR")
+    def perm(a,b):
+        if a >= b:
+            return(calculator.comb(a, b)*calculator.factorial(b))
+        else:
+            return("MATHS ERROR")
+    
 
 
