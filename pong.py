f541d7c62cca91b264dd01dbf49cf0cdde146431
Ike Holy Destiny <ikedestiny18@gmail.com>
2023-02-26 17:37:10 +0300

    made my first game PONGgit statusgit status!

---
+++ pong.py
@@ -0,0 +1,130 @@
+import turtle
+import winsound
+
+win = turtle.Screen()
+win.title("Pong by @ike_the_vinci")
+win.bgcolor("green")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Paddle a
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("black")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+#Paddle b
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("black")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid=1, stretch_len=1)
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.8
+ball.dy = -0.8
+
+#Pen 
+pen = turtle.Turtle()
+pen.speed = (0)
+pen.color("black")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0   Player B: 0", align="center", font=("Courier", 24, "bold"))
+
+
+score_a = 0
+score_b = 0
+
+
+#Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+win.listen()
+win.onkeypress(paddle_a_up, "w")
+win.onkeypress(paddle_a_down, "s")
+win.onkeypress(paddle_b_up, "Up")
+win.onkeypress(paddle_b_down, "Down")
+
+
+#Main game loop
+while True:
+    win.update()
+
+    #move ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}   Player B: {score_b}", align="center", font=("Courier", 24, "bold"))
+        
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}   Player B: {score_b}", align="center", font=("Courier", 24, "bold"))
+        
+    #ball and paddle collision settings
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+        ball.dx *= -1
+        ball.setx(340)
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+        
+        
+
+
+
+
+
+
